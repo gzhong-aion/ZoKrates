e582458e8c4a5aa3abc46e8f96a352d41605f25e
ali-sharif <ali@aion.network>
2019-11-03 10:18:27 -0500

    added event logs to g16 verifier

--- zokrates_core/src/proof_system/bn128/g16.rs
+++ zokrates_core/src/proof_system/bn128/g16.rs
@@ -537,12 +537,18 @@ public class Verifier {
 
     @Callable
     public static boolean verify(BigInteger[] input, byte[] proof) {
+        Blockchain.println("verify() called");
+
         try {
-            return verify(input, Proof.deserialize(proof));
+            if (verify(input, Proof.deserialize(proof))) {
+                Blockchain.log("VerifySnark".getBytes(), BigInteger.ONE.toByteArray());
+                return true;
+            }
         } catch (Exception e) {
             Blockchain.println("verify() failed with exception: " + e.getMessage());
         }
 
+        Blockchain.log("VerifySnark".getBytes(), BigInteger.ZERO.toByteArray());
         return false;
     }
 }
